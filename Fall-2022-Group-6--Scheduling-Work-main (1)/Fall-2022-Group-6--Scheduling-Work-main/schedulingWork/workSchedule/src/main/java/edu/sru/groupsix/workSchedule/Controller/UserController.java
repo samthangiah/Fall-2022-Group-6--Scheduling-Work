78877436af48de3701c6bc82e1d1ea5723f05e18
jxl1089 <111788751+jxl1089@users.noreply.github.com>
2022-09-12 00:22:05 -0400

    Add files via upload
    create the list tab and add account tab

---
+++ Fall-2022-Group-6--Scheduling-Work-main (1)/Fall-2022-Group-6--Scheduling-Work-main/schedulingWork/workSchedule/src/main/java/edu/sru/groupsix/workSchedule/Controller/UserController.java
@@ -0,0 +1,37 @@
+package edu.sru.groupsix.workSchedule.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/Email")
+	public String email() {
+		return "email.html";
+	}
+	
+	@RequestMapping("/emailissue")
+	public String emailissue() {
+		return "emailissue.html";
+	}
+	
+	@RequestMapping("/computer")
+	public String computer() {
+		return "computer.html";
+	}
+	
+	@RequestMapping("/socomputer")
+	public String socomputer() {
+		return "socomputer.html";
+	}
+	
+	@RequestMapping("/Campuse")
+	public String campus() {
+		return "Campuse.html";
+	}
+	@RequestMapping("/account")
+	public String account() {
+		return "account.html";
+	}
+}
---
+++ Fall-2022-Group-6--Scheduling-Work-main (1)/Fall-2022-Group-6--Scheduling-Work-main/schedulingWork/workSchedule/src/test/java/edu/sru/groupsix/workSchedule/WorkScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sru.groupsix.workSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
